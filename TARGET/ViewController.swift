dc2db7e6ca5628eaef539a3b4a46247edadb7e90
maxxx <kobzantsev.max@gmail.com>
2020-02-02 14:26:23 +0200

    Initial Commit

---
+++ TARGET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TARGET
+//
+//  Created by maxxx on 02.02.2020.
+//  Copyright © 2020 maxxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
